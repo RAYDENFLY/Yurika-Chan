bd6d5f46a5842859e5fe75141e90e27fd01f203e
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2021-04-01 22:43:46 +0700

    Create changemymind.js

---
+++ commands/meme/changemymind.js
@@ -0,0 +1,18 @@
+const Canvacord = require("canvacord/src/Canvacord")
+const { MessageAttachment } = require("discord.js")
+
+module.exports = {
+    name: 'changemymind',
+    description: "Sends a Customized Change My Mind meme",
+    usage: "?changemymind <text>",
+    category: "meme",
+    run: async(client, message, args) => {
+        let text = args.join(" ")
+
+        let image =  await Canvacord.changemymind(text)
+
+        let ChangeMyMind = new MessageAttachment(image, "cmm.png")
+
+        message.channel.send(ChangeMyMind)
+    }
+}
